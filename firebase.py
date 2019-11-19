d6251995eb8b4abe86202ab076ff47c97f3dc84e
Riccardo Torrisi <r.torrisi96@gmail.com>
2019-11-19 14:31:00 +0100

    Add firebase support

---
+++ firebase.py
@@ -0,0 +1,19 @@
+import pyrebase
+
+class Firebase:
+    def __init__(self, config):
+        firebase_config = {
+            "apiKey": config['apiKey'],
+            "authDomain": config['authDomain'],
+            "databaseURL": config['databaseURL'],
+            "storageBucket": config['storageBucket']
+        }
+
+        self.firebase = pyrebase.initialize_app(firebase_config)
+        self.database = self.firebase.database()
+        self.storage = self.firebase.storage()
+        self.auth = self.firebase.auth()
+        self.user = self.auth.sign_in_with_email_and_password(config['email'], config['password'])
+
+    def downloadFile(self, source_path, destination_path):
+        self.storage.child(source_path).download(destination_path)
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from RFID import RFIDReader
+from firebase import Firebase
+from config import config
+
+class WorkoutPlansManager:
+    def __init__(self):
+        self.firebase = Firebase(config)
+        self.rfidReader = RFIDReader('COM6', handler_function=self.handler)
+
+    def handler(self, card_no, state):
+        if state:
+            node = "users/"+card_no+"/untitled.jpg"
+            self.firebase.downloadFile(node, "storage_data/"+card_no+".jpg")
+        
+        print("handler -> card %s (%u)" % (card_no, state))
+
+    def run(self):    
+        self.rfidReader.start() 
+
+        while True:
+            try:
+                pass
+            except KeyboardInterrupt:
+                break
+        
+        self.rfidReader.stop()
+
 
 if __name__ == "__main__":
-    rfid_reader = RFIDReader('COM6')
-    rfid_reader.start() 
-
-    while True:
-        try:
-            pass
-        except KeyboardInterrupt:
-            break
-    
-    rfid_reader.stop()
+    WPM = WorkoutPlansManager()
+    WPM.run()
