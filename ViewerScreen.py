329444536a9b39f37fcbcacac8008a3b26f36995
Riccardo Torrisi <r.torrisi96@gmail.com>
2019-12-21 16:59:28 +0100

    Add zoom feature, extend timeout

--- ViewerScreen.py
+++ ViewerScreen.py
@@ -41,9 +41,10 @@ class ViewerScreen(Screen):
         self.event = None
         self.user_data = None
         self.page = 0
+        self.default_zoom = [1, 2.79]
         self.num_pages = 0
         self.saveUserDataCallback = saveFunc
-        self.timeout = 20
+        self.timeout = 30
 
         box_layout = BoxLayout(
             orientation='vertical'
@@ -61,7 +62,7 @@ class ViewerScreen(Screen):
         )
         self.label = Label(
             size_hint=(1, 1),
-            font_size=30,
+            font_size=20,
             italic=True,
             text=self.label_text
         )
@@ -80,7 +81,6 @@ class ViewerScreen(Screen):
             background_normal='app_data/close_normal.png',
             background_down='app_data/close_down.png',
             border=(0,0,0,0),
-            on_press=self.on_press_close_button,
             on_release=self.on_release_close_button
         )
         self.left_button = Button(
@@ -104,6 +104,28 @@ class ViewerScreen(Screen):
             background_down='app_data/right_down.png',
             border=(0,0,0,0),
             on_release=self.on_release_right_button
+        )
+        self.zoomOut_button = Button(
+            size_hint=(None, None),
+            width=70,
+            height=70,
+            valign='center',
+            halign='center',
+            background_normal='app_data/zoomOut_normal.png',
+            background_down='app_data/zoomOut_down.png',
+            border=(0,0,0,0),
+            on_release=self.on_release_zoomOut_button
+        )
+        self.zoomIn_button = Button(
+            size_hint=(None, None),
+            width=70,
+            height=70,
+            valign='center',
+            halign='center',
+            background_normal='app_data/zoomIn_normal.png',
+            background_down='app_data/zoomIn_down.png',
+            border=(0,0,0,0),
+            on_release=self.on_release_zoomIn_button
         )
         anchor_layout = AnchorLayout(
             size_hint=(1, 0.8),
@@ -132,6 +154,8 @@ class ViewerScreen(Screen):
 
         bar_panel.add_widget(image)
         bar_panel.add_widget(self.label)
+        bar_panel.add_widget(self.zoomOut_button)
+        bar_panel.add_widget(self.zoomIn_button)
         bar_panel.add_widget(self.left_button)
         bar_panel.add_widget(self.labelPage)
         bar_panel.add_widget(self.right_button)
@@ -149,24 +173,35 @@ class ViewerScreen(Screen):
 
     def on_enter(self):
         self.img_view = Image(
-            size_hint=(1, None),
-            height=1450,
+            size_hint=(self.default_zoom[0], self.default_zoom[1]),
+            allow_stretch = True,
             nocache=True,
             source=self.src
         )
+        self.img_view.bind(size_hint=self.on_img_hint_update)
         self.scrollview.add_widget(self.img_view)
         self.page = self.user_data['page']
         self.num_pages = self.user_data['num_pages']
         self.labelPage.text=str(self.page+1)+"/"+str(self.num_pages)
-        self.slider_value = self.user_data['slider']
         self.label_text = "Scheda di "+self.user_data['name']+" "+self.user_data['surname']
         self.setSourcePath("storage_data/"+str(self.user_data['rfid'])+"/scheda_"+str(self.page)+".jpg")
+        try:
+            self.slider_value = self.user_data['slider_y']
+            self.scrollview.scroll_x = self.user_data['slider_x']
+            zoom = self.user_data['zoom']
+            if zoom[0] == 0 and zoom[1] == 0:
+                zoom = self.default_zoom
+            self.img_view.size_hint = (zoom[0], zoom[1])
+        except:
+            self.slider_value = 1
+            self.scrollview.scroll_x = 0.5
+            self.img_view.size_hint = (self.default_zoom[0], self.default_zoom[1])
         self.reschedule()
 
     def on_pre_leave(self):
         Clock.unschedule(self.event)
         if self.saveUserDataCallback and self.user_data :
-            self.saveUserDataCallback(self.user_data['rfid'], {"slider": self.slider_value, "page":self.page})
+            self.saveUserDataCallback(self.user_data['rfid'], {"page": self.page, "zoom": self.img_view.size_hint, "slider_y": self.slider_value, "slider_x": self.scrollview.scroll_x})
     
     def on_leave(self):
         self.scrollview.remove_widget(self.img_view)
@@ -186,9 +221,6 @@ class ViewerScreen(Screen):
             self.slider_value = value
             self.reschedule()
 
-    def on_press_close_button(self, instance):
-        pass
-
     def on_release_close_button(self, instance):
         self.go_to_home()
 
@@ -200,6 +232,25 @@ class ViewerScreen(Screen):
             self.labelPage.text=str(self.page+1)+"/"+str(self.num_pages)
             self.setSourcePath("storage_data/"+str(self.user_data['rfid'])+"/scheda_"+str(self.page)+".jpg")
             self.slider_value = 1
+
+    def on_img_hint_update(self, instance, value):
+        scroll_x = self.scrollview.scroll_x
+        self.scrollview.scroll_x = scroll_x
+        self.reschedule()
+
+    def on_release_zoomOut_button(self, instance):
+        if self.img_view.size_hint_x / 1.2 >= 1:
+            if self.img_view:
+                self.img_view.size_hint = (self.img_view.size_hint_x / 1.2, self.img_view.size_hint_y / 1.2)
+                self.img_view.width = 0
+        else:
+            self.scrollview.scroll_x = 0.5
+
+    def on_release_zoomIn_button(self, instance):
+        if self.img_view.size_hint_x * 1.2 <= 3:
+            if self.img_view:
+                self.img_view.size_hint = (self.img_view.size_hint_x * 1.2, self.img_view.size_hint_y * 1.2)
+                self.img_view.width = 0
 
     def on_release_right_button(self, instance):
         self.reschedule()
